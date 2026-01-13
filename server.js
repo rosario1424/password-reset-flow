2a4749f8cc3b48ca899015a257c9159866b2d290
rosario1424 <rosariocse24@gmail.com>
2026-01-13 11:28:41 +0530

    Server.js is updated

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import { MONGODB_URI, PORT } from './utils/config.js';
+import app from './app.js';
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    const server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+    server.on('error', (err) => {
+      console.error('Server error:', err.message);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+
+
+
+
+/*const mongoose = require('mongoose');
 const { MONGODB_URI, PORT } = require('./utils/config');
 const app = require('./app');
 
@@ -18,4 +43,4 @@ mongoose
     })
     .catch(error => {
         console.error('Error connecting to MongoDB:', error.message);
-    })
+    }) */
